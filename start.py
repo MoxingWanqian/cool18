2f7d2b578edbea071ebf1c7bc55e91a7407baa99
moxingwanqian <moxingwanqian@hotmail.com>
2024-11-17 19:52:23 +0800

    update start.py

--- start.py
+++ start.py
@@ -1,17 +1,37 @@
 import os
 import sys
 
-print(os.path.dirname(sys.argv[0]))
+cwd = os.path.dirname(sys.argv[0])
 
 class CMD:
 
     def __init__(self):
         while True:
-            command = input('> ')
-            if command in ['quit', 'quit()', 'exit', 'exit()']:
+            command = input('> ').split(' ')
+            if command[0] in ['quit', 'quit()', 'exit', 'exit()']:
                 break
-            elif command.split(' ')[0] == 'search':
+            elif command[0] == 'help':
                 pass
+            elif command[0] == 'setting':
+                pass
+            elif command[0] == 'search':
+                pass
+            elif command[0] == 'download':
+                pass
+            elif command[0] == 'update':
+                pass
+        
+    def help(self, command:list):
+        pass
+
+    def search(self, command:list):
+        pass
+
+    def download(self, command:list):
+        pass
+
+    def update(self, command:list):
+        pass
 
 if __name__ == '__main__':
     cmd = CMD()
